48334b7ff1bf340989440c4eb530b4fd84905f8f
Janek <jang.lachowicz@gmail.com>
2016-02-18 21:42:51 +0100

    ...but forgot about tests

--- spec/policies/request_policy_spec.rb
+++ spec/policies/request_policy_spec.rb
@@ -1,28 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe RequestPolicy do
-
   let(:user) { User.new }
-
+  let(:mystery) { Mystery.new(admin: user) }
   subject { described_class }
 
-  permissions ".scope" do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-
-  permissions :show? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-
-  permissions :create? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-
-  permissions :update? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-
   permissions :destroy? do
-    pending "add some examples to (or delete) #{__FILE__}"
+    it 'denies access if user is not an author of a request' do
+      expect(subject).not_to permit(user, Request.new)
+    end
+
+    it 'allows access if user is an author of a request' do
+      expect(subject).to permit(user, Request.new(user: user))
+    end
   end
 end
